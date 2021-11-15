79380e1599afe7aa687505e7643bd8a94d70b598
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-16 10:30:09 +1100

    Added models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isNumeric: true,          // will only allow numbers
+        isDecimal: true,          // will only allow decimal
+      },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      default: 10,
+      validate: {
+        isNumeric: true,          // will only allow numbers
+        isInt: true,              // checks for valid integers
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false,
+      }
+    },
+    }
   },
   {
     sequelize,
